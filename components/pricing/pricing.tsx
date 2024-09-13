ec27e47a6cbe4a3d87be51fa66c85055f6c3f110
Rafael Leite <rafael.lprudencio1@gmail.com>
2024-09-13 14:17:48 -0300

    correcao no typebot para utilizar o nextjs

--- components/pricing/pricing.tsx
+++ components/pricing/pricing.tsx
@@ -1,35 +1,17 @@
 import { Section } from "components/section";
 import React from "react";
-import dynamic from 'next/dynamic';
+import { Standard } from '@typebot.io/nextjs';
 
-// Define the PricingProps interface
 interface PricingProps {
   children?: React.ReactNode;
-  plans?: any; // Replace 'any' with a specific type if available
+  plans?: any;
   title?: string;
   description?: string;
   [key: string]: any;
 }
 
-// Define the props for the Standard component
-interface StandardProps {
-  typebot: string;
-  style?: React.CSSProperties;
-  className?: string;
-  // Include any other required props
-}
-
 export const Pricing: React.FC<PricingProps> = (props) => {
   const { children, plans, title, description, ...rest } = props;
-
-  const Standard = dynamic(
-    () =>
-      import('@typebot.io/react').then(
-        (mod) => mod.Standard as React.ComponentType<StandardProps>
-      ),
-    { ssr: false }
-  );
-  
 
   return (
     <Section id="pricing" pos="relative" {...rest}>
